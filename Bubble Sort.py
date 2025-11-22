9453a716579fe33a2b112e03d8f9e0d2be1a9a27
Dev-Moynul <stu.moynulislam@gmail.com>
2025-11-22 19:43:01 +0600

    Bubble sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -2,10 +2,8 @@ def bubble_sort(arr):
     n = len(arr)
     
     for i in range(n):
-        # Last i elements are already in place
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
-                # swap
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 numbers = [5, 2, 9, 1, 5, 6]
 bubble_sort(numbers)
